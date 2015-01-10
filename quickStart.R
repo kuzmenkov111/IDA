7eb87a26b9abf289a2331285a1aa7f94fa10cf5b
emjaehyeon-kim/em <emdottami@gmail.com/em>
2015-01-10 19:19:44 +1100

    2015-01-10
    git status

---
+++ quickStart.R
@@ -0,0 +1,14 @@
+library(mlr)
+data(iris)
+
+## defind the task
+task = makeClassifTask(id="tutorial", data=iris, target="Species")
+
+## define the learner
+lrn = makeLearner("classif.lda")
+
+## define the resampling strategy
+rdesc = makeResampleDesc(method="CV", stratify=TRUE)
+
+## do resampling
+r = resample(task=task, learner=lrn, resampling=rdesc, show.info=FALSE)
