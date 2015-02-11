0bf6c4a5afd1645821a8627b6f7af696e06fccfb
Jaehyeon Kim <dottami@gmail.com>
2015-02-11 16:50:21 +1100

    2015-02-11

--- src/regCM.R
+++ src/regCM.R
@@ -1,43 +1,117 @@
 source("src/mlUtils.R")
 set.seed(1)
-data = rnorm(100,0,1)
-fit = rnorm(100,1,1)
-probs = c(0.9)
+data = rnorm(20,0,1)
+fit = rnorm(20,1,1)
+probs = c(0.5)
+
+probs = sort(probs)
+probsUp = probs
+if(probsUp[1]>0) probsUp = c(0,probsUp)
+if(probsUp[length(probsUp)]<1) probsUp = c(probsUp,1)
+qval = quantile(data, probsUp, na.rm=TRUE)
+
+ins = c()
+for(i in 1:length(data)) {
+  for(j in 1:length(probsUp)) {
+    if(data[i] <= qval[j]) {
+      ins = c(ins,probsUp[j])
+      break
+    }
+  }
+}
+
+conv = function(ins, probs) {
+  if(length(probs)==1) {
+    outs = c()
+    tmp = ins[ins<=probs[1]]
+    tmp[tmp<=probs[1]] = probs[1]
+    tmp = paste0(tmp*100,"%-")
+    outs = c(outs,tmp)
+    
+    tmp = ins[ins>probs[1]]
+    tmp[tmp>probs[1]] = probs[1]
+    tmp = paste0(tmp*100,"%+")
+    outs = c(outs,tmp)
+  } else {
+    
+  }
+  outs
+}
+
+outs = c()
+tmp = ins[ins<=probs[1]]
+tmp[tmp<=probs[1]] = probs[1]
+tmp = paste0(tmp*100,"%-")
+outs = c(outs,tmp)
+
+tmp = ins[ins>probs[1]]
+tmp[tmp>probs[1]] = probs[1]
+tmp = paste0(tmp*100,"%+")
+outs = c(outs,tmp)
+
+table(outs,outs)
+
+conv = function(data, probs) {
+  probsUp = sort(probs)
+  # update 0 and 1 if necessary
+  if(probsUp[1]>0) probsUp = c(0,probsUp)
+  if(probsUp[length(probsUp)]<1) probsUp = c(probsUp,1)
+  quans = quantile(actual, probsUp, na.rm=TRUE)      
+  dat = c()
+  for(i in 1:length(data)) {
+    for(j in 1:length(probsUp)) {
+      if(data[i] <= quans[j]) {
+        dat = c(dat,probsUp[j])
+        break
+      }
+    }
+  }
+  
+  datUp = dat
+  tmp1 = datUp[datUp <= probs[1]]
+  tmp1 = paste0("<=",probs[1]*100,"%")
+  tmp2 = datUp[datUp > probs[1]]
+  tmp2 = paste0(">",probs[1]*100,"%")
+  datUp = c(tmp1,tmp2)
+  
+  cbind(dat, datUp)
+} 
+
+
+
+
+
+
+
+
+
 
 getRegCM = function(actual, fitted, probs, type="Pred") {
-  if(length(probs) < 1) {
-    message("probs not set")
-  } else if(class(probs[1]) != "numeric") {
-    message("probs not numeric")
-  } else if(max(probs) > 1 | min(probs) < 0) {
-    message("probs not in valid range")
-  } else {    
-    conv = function(data, probs) {
-      probsUp = sort(probs)
-      # update 0 and 1 if necessary
-      if(probsUp[1]>0) probsUp = c(0,probsUp)
-      if(probsUp[length(probsUp)]<1) probsUp = c(probsUp,1)
-      quans = quantile(actual, probsUp, na.rm=TRUE)      
-      dat = c()
-      for(i in 1:length(data)) {
-        for(j in 1:length(probsUp)) {
-          if(data[i] <= quans[j]) {
-            dat = c(dat,probsUp[j])
-            break
-          }
+  conv = function(data, probs) {
+    probsUp = sort(probs)
+    # update 0 and 1 if necessary
+    if(probsUp[1]>0) probsUp = c(0,probsUp)
+    if(probsUp[length(probsUp)]<1) probsUp = c(probsUp,1)
+    quans = quantile(actual, probsUp, na.rm=TRUE)      
+    dat = c()
+    for(i in 1:length(data)) {
+      for(j in 1:length(probsUp)) {
+        if(data[i] <= quans[j]) {
+          dat = c(dat,probsUp[j])
+          break
         }
       }
-      
-      datUp = dat
-      tmp1 = datUp[datUp <= probs[1]]
-      tmp1 = paste0("<=",probs[1]*100,"%")
-      tmp2 = datUp[datUp > probs[1]]
-      tmp2 = paste0(">",probs[1]*100,"%")
-      datUp = c(tmp1,tmp2)
-      
-      cbind(dat, datUp)
-    }    
-    actualConv = conv(actual, probs)
-  }
+    }
+    
+    datUp = dat
+    tmp1 = datUp[datUp <= probs[1]]
+    tmp1 = paste0("<=",probs[1]*100,"%")
+    tmp2 = datUp[datUp > probs[1]]
+    tmp2 = paste0(">",probs[1]*100,"%")
+    datUp = c(tmp1,tmp2)
+    
+    cbind(dat, datUp)
+  }    
+  actualConv = conv(actual, probs)
   actualConv
 }
