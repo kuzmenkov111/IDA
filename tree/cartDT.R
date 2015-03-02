13700bf07f3293160a5ec2bd7f9cd21e6309af10
emjaehyeon-kim/em <emdottami@gmail.com/em>
2015-03-03 07:15:31 +1100

    2015-03-03

--- tree/cartDT.R
+++ tree/cartDT.R
@@ -3,9 +3,9 @@
 # test error
 ## individual error
 # non-parametric bootstrap mean approximately equals to non-informative posterior distribution
-# CI-like interval may be guessed from the distribution of individual trees
+# CI-like interval may be guessed from the distribution of the errors of individual trees
 ## cumulative error
-# relieving concern of over-estimation by averaging or majority voting
+# relieving concern of overfitting by averaging or majority voting
 # more reliable prediction
 ## variable importance
 # if single tree's variable importance is far different from that of bagged trees
@@ -27,57 +27,91 @@ source("src/cart.R")
 require(caret)
 set.seed(1237)
 trainIndex = createDataPartition(Carseats$High, p=0.8, list=FALSE, times=1)
-trainData.rg = data.rg[trainIndex,]
-testData.rg = data.rg[-trainIndex,]
+trainData.cl = data.cl[trainIndex,]
+testData.cl = data.cl[-trainIndex,]
 
 ## instantiate rpartDT
 set.seed(12357)
-rg = cartDT(trainData.rg, testData.rg, "Sales ~ .", ntree=10)
+cl = cartDT(trainData.cl, testData.cl, "High ~ .", ntree=10)
 
 # class and names
-class(rg)
+class(cl)
 # rpt - single tree, lst - least xerror, se - 1-SE rule, oob - out-of-bag sample, tst - test data, ind (cum) - individual (cumulative) prediction or error
-names(rg)
+names(cl)
 
 ## cp values
 # cart
-round(t(data.frame(c(rg$rpt$cp[1,][2],rg$rpt$cp[1,][1]))),4)
+cl$rpt$cp[1,][[1]]
 # bagging
-round(rg$boot.cp[,1:5],4)
+summary(t(cl$boot.cp)[,2])
 
-## errors
-crt.err = data.frame(train.lst.err=rg$rpt$train.lst$error$error
-                     ,test.lst.err=rg$rpt$test.lst$error$error)
+## fitted values
+# individual - each oob sample
+cl$ind.oob.lst[3:6,1:8]
 
-## individual errors
-# cart train error
-crt.err[1]
+# cumulative - majority vote or average
+# 1. not used - NA, 2. used once - get name, 3. tie - NA, 4. name at max number of labels
+cl$cum.oob.lst[3:6,1:8]
 
-# oob error at least xerror - se to see 1-SE rule
-# only 10 trees, 0 is not included but not a big deal
-summary(unlist(rg$ind.oob.lst.err))
+# function to update fitted values - majority vote or average
+# response kept in 1st column, should be excluded
+retCum = function(fit) {
+  if(ncol(fit) < 2) {
+    message("no fitted values")
+    cum.fit = fit
+  } else {
+    cum.fit = as.data.frame(apply(fit,2,function(x) { rep(0,times=(nrow(fit))) }))
+    cum.fit[,1:2] = fit[,1:2]
+    rownames(cum.fit) = rownames(fit)
+    for(i in 3:ncol(fit)) {
+      if(class(fit[,1])=="factor") {
+        retVote = function(x) {
+          tbls = apply(x,1,as.data.frame)
+          tbls = lapply(tbls,table)
+          ret = function(x) {
+            if(length(x)==0) NA 
+            else if(length(x)==1) names(x) 
+            else if(max(x)==min(x)) NA 
+            else names(x[x==max(x)])
+          }
+          maxVal = sapply(tbls,ret)
+          maxVal
+        }
+        cum.fit[,i] = retVote(fit[,2:i]) # retVote already vectorized
+      } else {
+        cum.fit[,i] = apply(fit[,2:i],1,mean,na.rm=TRUE)
+      }
+    }  
+  }
+  cum.fit
+}
 
-# cart test error
-crt.err[2]
+# function to updated errors - mmce or rmse
+# response kept in 1st column, should be excluded
+retErr = function(fit) {
+  err = data.frame(t(rep(0,times=ncol(fit))))
+  colnames(err)=colnames(fit)
+  for(i in 2:ncol(fit)) {
+    cmpt = complete.cases(fit[,1],fit[,i])
+    if(class(fit[,1])=="factor") {
+      tbl=table(fit[cmpt,1],fit[cmpt,i])
+      err[i] = 1 - sum(diag(tbl))/sum(tbl)
+    } else {
+      err[i] = sqrt(sum(fit[cmpt,1]-fit[cmpt,i])^2/length(fit[cmpt,1]))
+    }    
+  }
+  err[2:length(err)]
+}
 
-# individual test error
-summary(unlist(rg$ind.tst.lst.err))
-
-## cumulative errors
-# oob error
-summary(unlist(rg$cum.oob.lst.err))
-
-# test error
-summary(unlist(rg$cum.tst.lst.err))
+cl$ind.oob.lst.err[1:7]
+cl$cum.oob.lst.err[1:7]
 
 ## variable importance
-cart.varImp = data.frame(method="cart"
-                         ,variable=names(rg$rpt$mod$variable.importance)
-                         ,value=rg$rpt$mod$variable.importance/sum(rg$rpt$mod$variable.importance)
-                         ,row.names=NULL)
-# bagging - lst to se to see 1-SE rule
-bgg.varImp = data.frame(method="bagging"
-                        ,variable=rownames(rg$cum.varImp.lst)
-                        ,value=rg$cum.varImp.lst[,10])
-rg.varImp = rbind(cart.varImp,bgg.varImp)
-rg.varImp
+# cart
+data.frame(variable=names(cl$rpt$mod$variable.importance)
+           ,value=cl$rpt$mod$variable.importance/sum(cl$rpt$mod$variable.importance)
+           ,row.names=NULL)
+# bagging - cumulative
+ntree = 10
+data.frame(variable=rownames(cl$cum.varImp.lst)
+           ,value=cl$cum.varImp.lst[,ntree])
--- tree/part5_stab.R
+++ tree/part5_stab.R
@@ -10,78 +10,64 @@ data.rg = subset(Carseats, select=c(-High))
 ## import constructors
 source("src/cart.R")
 
-# split - cl: classification, rg: regression
+# split data
 require(caret)
 set.seed(1237)
 trainIndex = createDataPartition(Carseats$High, p=0.8, list=FALSE, times=1)
 trainData.cl = data.cl[trainIndex,]
 testData.cl = data.cl[-trainIndex,]
 
-## instantiate rpartDT
+## run rpartDT
 set.seed(12357)
 cl = cartDT(trainData.cl, testData.cl, "High ~ .", ntree=1000)
 
-# class and names
-class(cl)
-names(cl)
-
 ## cp values
 # cart
-round(t(data.frame(c(cl$rpt$cp[1,][2],cl$rpt$cp[1,][1]))),4)
+cl$rpt$cp[1,][[1]]
 # bagging
-round(cl$boot.cp[,1:5],4)
-
-## errors
-crt.err = data.frame(train.lst.err=cl$rpt$train.lst$error$error
-                     ,test.lst.err=cl$rpt$test.lst$error$error)
+summary(t(cl$boot.cp)[,2])
 
 ## individual errors
-## single tree's train and test errors are quite lower
-## for oob error, 3 sd away / for test error, 1.4 sd away
-## if the mean of oob errors approximately equals to poterior means, the train error rate of the single tree may be unreasonable
-(mean(unlist(cl$ind.oob.lst.err)) - crt.err[[1]])/sd(unlist(cl$ind.oob.lst.err))
-(mean(unlist(cl$ind.tst.lst.err)) - crt.err[[2]])/sd(unlist(cl$ind.tst.lst.err))
-
-# cart train error
-crt.err[1]
-
-# oob error at least xerror - se to see 1-SE rule
-summary(unlist(cl$ind.oob.lst.err))
-
-ind.oob.error = data.frame(error=unlist(cl$ind.oob.lst.err))
-ggplot(ind.oob.error, aes(x=error)) + 
-  geom_histogram() + geom_vline(xintercept=crt.err[[1]], color="blue")
-
 # cart test error
-crt.err[2]
+crt.err = cl$rpt$test.lst$error$error
+crt.err
 
-# individual test error
-summary(unlist(cl$ind.tst.lst.err))
+## single tree's train and test errors are quite lower
+## for oob error, 2.3 sd away / for test error, 1.4 sd away
+## if the mean of oob errors approximately equals to poterior means, the train error rate of the single tree may be unreasonable
+(mean(unlist(cl$ind.oob.lst.err)) - crt.err)/sd(unlist(cl$ind.oob.lst.err))
+(mean(unlist(cl$ind.tst.lst.err)) - crt.err)/sd(unlist(cl$ind.tst.lst.err))
 
-ind.tst.error = data.frame(error=unlist(cl$ind.tst.lst.err))
-ggplot(ind.tst.error, aes(x=error)) + 
-  geom_histogram() + geom_vline(xintercept=crt.err[[2]], color="blue")
+# bagging error at least xerror - se to see 1-SE rule
+ind.oob.err = data.frame(type="oob",error=unlist(cl$ind.oob.lst.err))
+ind.tst.err = data.frame(type="test",error=unlist(cl$ind.tst.lst.err))
+ind.err = rbind(ind.oob.err,ind.tst.err)
+ind.err.summary = as.data.frame(rbind(summary(ind.err$error[ind.err$type=="oob"])
+                                      ,summary(ind.err$error[ind.err$type=="test"]))) 
+rownames(ind.err.summary) <- c("oob","test")
+ind.err.summary
+
+# plot error distribution
+ggplot(ind.err, aes(x=error,fill=type)) + 
+  geom_histogram() + geom_vline(xintercept=crt.err, color="blue") + 
+  ggtitle("Error distribution") + theme(plot.title=element_text(face="bold"))
 
 ## cumulative errors
 ## for oob error, the bagging error stays around 0.225, which is well over the training error of 0.16
 ## for test error, the bagging error converges to around 0.164, which is lower than 0.19
 ## bagging doesn't overestimate and provided better prediction
-# oob error
-summary(unlist(cl$cum.oob.lst.err))
-
-bgg.oob.err = data.frame(sample=1:length(cl$cum.oob.lst.err)
+bgg.oob.err = data.frame(type="oob"
+                         ,ntree=1:length(cl$cum.oob.lst.err)
                          ,error=unlist(cl$cum.oob.lst.err))
-ggplot(data=bgg.oob.err,aes(x=sample,y=error)) + 
-  geom_line() + ylim(crt.err[[1]],max(bgg.oob.err$error)) + 
-  geom_abline(intercept=crt.err[[1]],slope=0,color="blue")
-
-# test error
-summary(unlist(cl$cum.tst.lst.err))
-
-bgg.tst.err = data.frame(sample=1:length(cl$cum.tst.lst.err)
+bgg.tst.err = data.frame(type="test"
+                         ,ntree=1:length(cl$cum.tst.lst.err)
                          ,error=unlist(cl$cum.tst.lst.err))
-ggplot(data=bgg.tst.err,aes(x=sample,y=error)) + 
-  geom_line() + geom_abline(intercept=crt.err[[2]],slope=0,color="blue")
+bgg.err = rbind(bgg.oob.err,bgg.tst.err)
+
+# plot bagging errors
+ggplot(data=bgg.err,aes(x=ntree,y=error,colour=type)) + 
+  geom_line() + geom_abline(intercept=crt.err,slope=0,color="blue") + 
+  ggtitle("Bagging error") + theme(plot.title=element_text(face="bold"))
 
 ## variable importance
 ## variable importance is somewhat different - US, Urban and Education are rarely considered in the single tree
@@ -90,7 +76,7 @@ cart.varImp = data.frame(method="cart"
                          ,variable=names(cl$rpt$mod$variable.importance)
                          ,value=cl$rpt$mod$variable.importance/sum(cl$rpt$mod$variable.importance)
                          ,row.names=NULL)
-# bagging - lst to se to see 1-SE rule
+# bagging
 ntree = 1000
 bgg.varImp = data.frame(method="bagging"
                         ,variable=rownames(cl$cum.varImp.lst)
@@ -98,7 +84,7 @@ bgg.varImp = data.frame(method="bagging"
 # plot variable importance measure
 cl.varImp = rbind(cart.varImp,bgg.varImp)
 cl.varImp$variable = reorder(cl.varImp$variable, 1/cl.varImp$value)
-ggplot(data=cl.varImp,aes(x=variable,y=value,fill=method)) + geom_bar(stat="identity")
-
+ggplot(data=cl.varImp,aes(x=variable,y=value,fill=method)) + geom_bar(stat="identity") + 
+  ggtitle("Variable importance") + theme(plot.title=element_text(face="bold"))
 
 
