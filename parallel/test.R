0084c6d8b7cb074b1bad774936f08b841149aa7b
Jaehyeon Kim <dottami@gmail.com>
2015-03-10 16:05:21 +1100

    2015-03-10

--- parallel/test.R
+++ parallel/test.R
@@ -37,11 +37,66 @@ x = foreach(a=1:1000, b=rep(10,2)) %do% (a+b)
 # .inorder - important if parallel
 x = foreach(i=1:3, .combine="c") %do% exp(i)
 x = foreach(i=1:3, .combine="cbind") %do% rnorm(4)
-myMean = function(x) { sum(x)/length(x) }
-x = foreach(i=1:4, .combine='c') %do% {
+x = foreach(i=1:3, .combine="c") %do% rnorm(4)
+x = foreach(i=1:4, .combine='cbind') %do% {
   rand = rnorm(4)
   sum(rand)/length(rand)
 }
+## foreach with iterators
+library(iterators)
+x = foreach(a = 1:3, .combine='cbind') %do% rnorm(4)
+# iterators allow the data to be generated on-the-fly, as it is needed by your operations
+# rather than requiring all of the data to be generated at the beginning.
+x = foreach(a=irnorm(4, count=3), .combine='cbind') %do% a
+set.seed(123)
+x = foreach(a=irnorm(4, count=1000), .combine='+') %do% a
+x = foreach(a=icount(1000), .combine='+') %do% rnorm(4)
+## parallel execution
+set.seed(123)
+x = matrix(runif(500), 100)
+y = gl(2, 50)
+library(randomForest)
+rf1 = foreach(ntree=rep(250, 4), .combine=combine) %do% randomForest(x, y, ntree=ntree)
+rf2 = foreach(ntree=rep(250, 4), .combine=combine, .packages='randomForest') %dopar% randomForest(x, y, ntree=ntree)
+
+applyKernel = function(newX, FUN, d2, d.call, dn.call=NULL, ...) {
+  ans = vector("list", d2)
+  for(i in 1:d2) {
+    tmp = FUN(array(newX[,i], d.call, dn.call))
+    if(!is.null(tmp)) ans[[i]] = tmp
+  }
+  ans
+}
+applyKernel = function(newX, FUN, d2, d.call, dn.call=NULL, ...) {
+  foreach(i=1:d2) %dopar%
+    FUN(array(newX[,i], d.call, dn.call), ...)
+}
+applyKernel = function(newX, FUN, d2, d.call, dn.call=NULL, ...) {
+  foreach(x=iter(newX, by='col')) %dopar%
+    FUN(array(x, d.call, dn.call), ...)
+}
+
+iblkcol <- function(a, chunks) {
+  n <- ncol(a)
+  i <- 1  
+  nextElem <- function() {
+    if (chunks <= 0 || n <= 0) stop('StopIteration')
+    m <- ceiling(n / chunks)
+    r <- seq(i, length=m)
+    i <<- i + m
+    n <<- n - m
+    chunks <<- chunks - 1
+    a[,r, drop=FALSE]
+  }  
+  structure(list(nextElem=nextElem), class=c('iblkcol', 'iter'))
+}
+nextElem.iblkcol <- function(obj) obj$nextElem()
+applyKernel = function(newX, FUN, d2, d.call, dn.call=NULL, ...) {
+  foreach(x=iblkcol(newX, 3), .combine='c', .packages='foreach') %dopar% {
+    foreach(i=1:ncol(x)) %do% FUN(array(x[,i], d.call, dn.call), ...)
+  }
+}
+applyKernel(matrix(1:16, 4), mean, 4, 4)
 
 
 
