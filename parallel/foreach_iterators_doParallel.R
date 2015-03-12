d1284e451c6d6e2a3ba2b59dd315aa22cfdc7b0b
emjaehyeon-kim/em <emdottami@gmail.com/em>
2015-03-12 22:24:01 +1100

    2015-03-12

---
+++ parallel/foreach_iterators_doParallel.R
@@ -0,0 +1,21 @@
+### foreach
+library(foreach)
+x = foreach(i=1:3) %do% sqrt(i)
+x
+
+x = foreach(a=1:3, b=rep(10,3)) %do% (a + b)
+x = foreach(a=1:3, b=rep(10,3)) %do% {
+  a + b
+}
+# iteration to min(length(a), length(b), ...)
+x = foreach(a=1:100, b=rep(10,3)) %do% (a + b)
+
+# .combine option
+x = foreach(a=1:3, .combine="c") %do% exp(a)
+x = foreach(a=1:3, .combine="rbind") %do% exp(a)
+x = foreach(a=1:3, .combine="+") %do% exp(a)
+x = foreach(a=1:3, .combine="min") %do% exp(a)
+
+
+
+
